86dfdb845821a4f94b97f8e4e22f2152bcde7ce6
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-06-07 22:43:52 -0300

    chore: overwriting eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    'react-refresh/only-export-components': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off'
+  }
 }
